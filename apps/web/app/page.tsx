b85cc0fea0a027769f50d8cc865cc07daf111714
HandikaDiaz <handikaalexandriadiaz@gmail.com>
2025-09-25 17:51:14 +0700

    Setup Clerk Authentication

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,29 @@
 "use client"
 import { useQuery } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
+import { Authenticated, Unauthenticated } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Page() {
   const users = useQuery(api.users.getMany)
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <p>App/web</p>
-      <div>
-        {JSON.stringify(users, null, 2)}
-      </div>
-    </div>
+    <>
+      <Authenticated>
+        <div className="flex flex-col items-center justify-center min-h-svh">
+          <p>App/web</p>
+          <UserButton />
+          <div>
+            {JSON.stringify(users, null, 2)}
+          </div>
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        <p>
+          Must be signed in
+        </p>
+        <SignInButton />
+      </Unauthenticated>
+    </>
   )
 }
